2fa4656f376cc2a6c95acc9a139c591cd4569cf6
Diego <81892372+Wise-II@users.noreply.github.com>
2026-01-09 14:16:18 -0500

    command loader partially built

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { Client, Events, GatewayIntentBits } from 'discord.js';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //end import, for now... 
 //spawns a new client instance
@@ -13,5 +15,14 @@ const client = new Client ({intents: [GatewayIntentBits.Guilds] });
 client.once(Events.ClientReady, (readyClient)=> {
     console.log(`ready! Signed in as ${readyClient.user.tag}`)
 });
+//load commands (time to blueball myself)
+const commandpaths = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandpaths).filter(f=>f.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = await import(`file://$filePath`);
+    client.command.set(command.data.name, command);
+
+}
+
 
 client.login(process.env.DISCORD_TOKEN);
